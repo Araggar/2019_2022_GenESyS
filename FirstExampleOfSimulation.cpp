f868cf863a61f2250499283d653af31c552d9f7d
Lucas Mayr de Athayde <lucas.mayr.athayde@gmail.com>
2021-03-30 11:28:03 -0300

    Add discrete

--- FirstExampleOfSimulation.cpp
+++ FirstExampleOfSimulation.cpp
@@ -75,7 +75,7 @@ int FirstExampleOfSimulation::main(int argc, char** argv) {
 	
 	// Discrete
 	sampler.reset();
-	file.open("Discrete(10, [4,1,2,3],4)-Output");
+	file.open("Discrete(10,[4,1,2,3],[1,2,3,4],4)-Output");
 	double values[4] = {1,2,3,4};
 	double prob[4] = {4,2,1,3};
 	for (int i = 0; i < 1000; i++) {
