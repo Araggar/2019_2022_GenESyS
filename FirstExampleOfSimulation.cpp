e7a9e1774addfed6efca61cf6f843516b22edfbd
Lucas Mayr de Athayde <lucas.mayr.athayde@gmail.com>
2021-03-30 15:59:14 -0300

    Update outputs

--- FirstExampleOfSimulation.cpp
+++ FirstExampleOfSimulation.cpp
@@ -113,6 +113,12 @@ int FirstExampleOfSimulation::main(int argc, char** argv) {
 		file << sampler.sampleGamma2(2.5,2) << std::endl;
 	}
 	file.close();
+	
+	file.open("Gamma(0.5,1)-Output");
+	for (int i = 0; i < 1000; i++) {
+		file << sampler.sampleGamma2(0.5,1) << std::endl;
+	}
+	file.close();
 
 	// Binomial & std::binomial_distribution
 	sampler.reset();
